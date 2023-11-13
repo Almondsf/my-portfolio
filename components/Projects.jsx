367bfbc4e3c1b322f8a0fab692f3055177a03f15
Oluwafemi Oloyede <oloyedeoluwafemi22@gmail.com>
2023-11-13 13:28:08 +0100

    ...updating project images

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,13 +2,31 @@ import Image from "next/image";
 import Github from "../assets/github";
 import LiveLink from "../assets/link";
 import Linkedin from "../assets/Linkedin";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Cafe from "../assets/cafe-desktop.png";
 
 const Projects = () => {
+  var carouselSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
   const projects = [
     {
       title: "Cinemania",
       desc: "I'm thrilled to present my latest project, an e-commerce website in development. Leveraging the capabilities of Next.js and Express.js, powered by JavaScript, this platform is designed to redefine onl...",
-      image: "",
+      image: [
+        {
+          imageOne: <Cafe />,
+        },
+      ],
       preview: "",
       repo: "",
     },
@@ -52,7 +70,18 @@ const Projects = () => {
             key={idx}
             className="bg-white text-black rounded-md p-4 flex flex-col gap-4 cursor-default"
           >
-            {/* <Image /> */}
+            <Slider {...carouselSettings}>
+              <div className="">
+                <Image
+                  src={Cafe}
+                  // alt={altText}
+                  className=""
+                  height={250}
+                  width={500}
+                  priority
+                />
+              </div>
+            </Slider>
             <h2 className="text-xl">{project.title}</h2>
             <p>{project.desc}</p>
             <div className="flex justify-between">
